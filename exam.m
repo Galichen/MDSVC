3438fdd2083ab05646c85eef359bf54199586ed5
Galichen <44158450+Galichen@users.noreply.github.com>
2021-01-11 14:28:33 +0800

    Create exam.m

---
+++ exam.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %% exam
+clc
+close all
+addpath(genpath('MSVC-CD'))
+%% %%%------------------------kernel MSVC----------------------%%%
+load('iris.mat');
+%% set parameters
+C = 0.01;q = 1;lambda1 = 0.5;lambda2 = 0.01;
+[SV,BSV,beta,alpha,quad,R,ClusterPerQ,clusters_assignments]  = LSVC(Samples,classification,C,q,lambda1,lambda2);
+labels_pre = clusters_assignments;
+%% evaluation
+RI = RandIndex(labels_pre,classification);
+AC = cluster_acc(classification,labels_pre);
