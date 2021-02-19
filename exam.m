2859a4391b79d1270533dc433a5fe187d0d26336
Galichen <44158450+Galichen@users.noreply.github.com>
2021-02-19 22:56:03 +0800

    Update exam.m

--- exam.m
+++ exam.m
@@ -5,9 +5,8 @@ addpath(genpath('MDSVC-CD'))
 %% %%%------------------------kernel MDSVC----------------------%%%
 load('iris.mat');
 %% set parameters
-C = 0.01;q = 1;lambda1 = 0.5;lambda2 = 0.01;
+C = 0.01;q = 0.25;lambda1 = 0.25;lambda2 = 0.01;
 [SV,BSV,beta,alpha,quad,R,ClusterPerQ,clusters_assignments]  = LSVC(Samples,classification,C,q,lambda1,lambda2);
 labels_pre = clusters_assignments;
 %% evaluation
 ARI = RandIndex(labels_pre,classification);
-ACC = cluster_acc(classification,labels_pre);
