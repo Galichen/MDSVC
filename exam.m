63637f50d7f63110a3e25832a0218653569bf9c4
Galichen <44158450+Galichen@users.noreply.github.com>
2021-04-02 09:43:46 +0800

    Update exam.m

--- exam.m
+++ exam.m
@@ -7,6 +7,6 @@ load('dbmoon.mat');
 %% set parameters
 C = 0.1;q = 0.1;lambda1 = 1;lambda2 = 4;
 %% Model and Visualization %%
-main(Samples, classification, C, q, lambda1,lambda2)；
+main(Samples, classification, C, q, lambda1,lambda2);
 
 
