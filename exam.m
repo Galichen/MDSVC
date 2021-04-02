0072e5a265502ca62948e588c9a02770a74ca649
Galichen <44158450+Galichen@users.noreply.github.com>
2021-04-02 09:42:33 +0800

    Update exam.m

--- exam.m
+++ exam.m
@@ -7,6 +7,6 @@ load('dbmoon.mat');
 %% set parameters
 C = 0.1;q = 0.1;lambda1 = 1;lambda2 = 4;
 %% Model and Visualization %%
-[SV,BSV,beta,alpha,quad,R]  = MDSVC(Samples,classification,C,q,lambda1,lambda2);
+main(Samples, classification, C, q, lambda1,lambda2)；
 
 
